239d3d1d583d127b0b98b550a14e9f435ddc11af
cruzjeff25 <cruzjeff254@gmail.com>
2022-10-14 18:58:44 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "../src/style.css"
+import logo from "./img/logo.png"
+import apk from "./LetsPark.apk"
+import mobile from "./img/Mobile.png"
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div className='nav'>
+        <div className='logo'>
+            <img src={logo} alt="logo" width="120px"/>
+            <h1>Let's Park!</h1>
+        </div>
+        <button><a href={apk} download>Download</a></button>
+      </div>
+    
+      <section className='sec-1'>
+        <div className='mobile-img'>
+          <img src={mobile} alt="mobile" width="350px" />
+        </div>
+      </section>
+
+      <section className='sec-2'>
+      </section>
+
+      <section className='sec-3'>
+      </section>
+
+      <section className='sec-4'>
+      </section>
+
+      <section className='sec-5'>
+      </section>
+
+      <footer>
+      </footer>
+    </>
   );
 }
-
-export default App;

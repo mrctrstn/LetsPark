6fa03cfa03578cea7fc9d51efaaa77326a269630
mrctrstn <95068642+mrctrstn@users.noreply.github.com>
2022-10-15 18:33:43 +0800

    unfinished
    sec1-sec3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "../src/style.css"
 import logo from "./img/logo.png"
 import apk from "./LetsPark.apk"
 import mobile from "./img/Mobile.png"
+import parking from "./img/Parking.png"
+import payment from "./img/Payment.png"
+import rent from "./img/Rent.png"
 
 
 
@@ -20,14 +23,60 @@ export default function App() {
     
       <section className='sec-1'>
         <div className='mobile-img'>
-          <img src={mobile} alt="mobile" width="350px" />
+          <img src={mobile} alt="mobile" width="360px" />
         </div>
+        <div class="text">
+          <h1>The app that will<br></br>
+          find your parking<br></br>
+          space.</h1>
+          <br></br>
+          <br></br>
+          <p>Parking with ease, seamless payment, and<br></br>
+          generate extra income. Meet the app will<br></br>
+          bring convenience in parking and will help you<br></br>
+          start your parking business.</p>
+          </div>
       </section>
 
       <section className='sec-2'>
+         <div class="row">
+          <div class="column">
+            <img src={parking} alt="parking" width="150px" height="125px" />
+           <h1>Book parking space</h1><br></br>
+            <p>Choose from large variety of<br></br>
+            parking space in Valenzuela. Book,<br></br> 
+            park, and carry on with your day.</p>
+          </div>
+          <div class="column">
+          <img src={payment} alt="payment" width="150px" height="125px" />
+          <h1>Pay with ease</h1><br></br>
+            <p>With Let's park payment is made<br></br>
+            easy. With few taps your parking is<br></br> 
+           ready to go.</p>
+          </div>
+          <div class="column"><img src={rent} alt="rent" width="150px" height="125px" />
+          <h1>BookRent out your space</h1><br></br>
+            <p>Generate extra income by renting<br></br>
+            out your space. Follow easy<br></br> 
+            instructions and make your day.</p>
+          </div>
+         </div>
+         
       </section>
 
       <section className='sec-3'>
+        <div className='logo-img'>
+          <img src={logo} alt="logo" width="500px"/>
+
+        </div>
+        <div class="text">
+         <h1>Download Let's Park now!</h1>
+          <br></br>
+          <p>Start by downloading our app and register with few and<br></br>
+          easy steps. Try and experience secured and seamless<br></br>
+          parking in Valenzuela using Let's Park!</p>
+          </div>
+          <button><a href={apk} download>Download</a></button>
       </section>
 
       <section className='sec-4'>
